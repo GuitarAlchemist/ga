74649b8785747dee34b2a34102868046583a4f91
spareilleux <spareilleux@gmail.com>
2022-10-21 16:20:18 -0400

    - Added GA.Business.Config.fsproj - GA.Business.Config.fsproj: Added Tunings.yaml - GA.Business.Core: Removed TuningInfo record, Made Tuning class no longer responsible for configuration (See TuningsConfig.fs) - Added notebooks for common scale modes

---
+++ Common/GA.Business.Core/Fretboard/Tuning.cs
@@ -0,0 +1,53 @@
+﻿namespace GA.Business.Core.Fretboard;
+
+using GA.Core;
+using Primitives;
+using Notes;
+
+/// <summary>
+/// Guitar tuning
+/// </summary>
+/// <remarks>
+/// References:
+/// https://www.guitarworld.com/lessons/11-alternate-tunings-every-guitarist-should-know
+/// https://www.stringsbymail.com/TuningChart.pdf
+/// </remarks>
+[PublicAPI]
+public class Tuning : IIndexer<Str, Pitch>
+{
+    private readonly IReadOnlyDictionary<Str, Pitch> _pitchByStr;
+
+    public Tuning(PitchCollection pitchCollection)
+    {
+        if (pitchCollection == null) throw new ArgumentNullException(nameof(pitchCollection));
+        PitchCollection = pitchCollection ?? throw new ArgumentNullException(nameof(pitchCollection));
+        _pitchByStr = PitchByStr(pitchCollection);
+    }
+
+    /// <summary>
+    /// Gets the <see cref="PitchCollection"/>
+    /// </summary>
+    public PitchCollection PitchCollection { get; }
+    public Pitch this[Str str] => _pitchByStr[str];
+
+    private static IReadOnlyDictionary<Str, Pitch> PitchByStr(IEnumerable<Pitch> pitches)
+    {
+        var pitchesList = pitches.ToImmutableList();
+        var lowestPitchFirst = pitchesList.First() < pitchesList.Last();
+        if (lowestPitchFirst) pitchesList = pitchesList.Reverse();
+
+        var str = Str.Min;
+        var dict = new Dictionary<Str, Pitch>();
+        foreach (var pitch in pitchesList)
+        {
+            dict[str++] = pitch;
+        }
+
+        // Result
+        var result = dict.ToImmutableDictionary();
+
+        return result;
+    }
+
+    public override string ToString() => PitchCollection.ToString() ?? string.Empty;
+}
