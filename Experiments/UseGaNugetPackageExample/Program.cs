01c707b3265eeea259e14f3b6c84ad4a455e52b8
Stephane Pareilleux <spareilleux@hotmail.com>
2022-11-24 21:06:40 -0500

    UseGaNugetPackageExample: Tested GuitarAlchemist.GA.Business.Core 1.0.10-beta1 (VariationsWithRepetitions class) - GA.Interactive.LocalNuget.csproj/Local NuGet Smote Test.ipynb: Tested VariationsWithRepetitions class - GA.Interactive.csproj: Incremented version to 1.0.1-beta3

--- Experiments/UseGaNugetPackageExample/Program.cs
+++ Experiments/UseGaNugetPackageExample/Program.cs
@@ -1,5 +1,8 @@
-﻿using GA.Business.Config;
+﻿using System.Collections.Immutable;
+using GA.Business.Config;
 using GA.Business.Core.Tonal.Modes;
+using GA.Business.Core.Fretboard.Primitives;
+using GA.Core.Combinatorics;
 
 var modes = MajorScaleMode.Objects;
 
@@ -12,6 +15,8 @@ foreach (var mode in modes)
     Console.WriteLine("");       
 }
 
+// -----------------------------------------------------------------
+
 var a = Instruments.Instrument;
 
 var instrument = Instruments.Instrument.Ukulele;
@@ -19,4 +24,40 @@ var instrument = Instruments.Instrument.Ukulele;
 foreach (var prop in instrument.GetType().GetProperties())
 {
     Console.WriteLine(prop.Name);
+}
+
+// -----------------------------------------------------------------
+
+var moveableVariations = new MoveableVariations();
+var normalForms = new List<Variation<Fret>>();
+foreach (var variation in moveableVariations)
+{
+    var index = moveableVariations.GetIndex(variation);
+
+    if (index != variation.Index) throw new InvalidOperationException("That sucks!");
+
+    var isNormalForm = variation.Any(fret => fret == Fret.One);
+    if (!isNormalForm) continue;
+    normalForms.Add(variation);
+}
+
+var variationsCount = moveableVariations.Count;
+var variationsArray  = moveableVariations.ToImmutableArray();
+
+// Console.WriteLine($"{string.Join(" ", movable)} => {variations.Count} variations");
+foreach (var variation in moveableVariations)
+{
+    Console.WriteLine(variation.ToString());
+}
+
+/// <summary>
+/// - Muted fret + frets 1..5;
+/// - 6 string
+/// </summary>
+public sealed class MoveableVariations : VariationsWithRepetitions<Fret>
+{
+    public MoveableVariations(int strCount = 6) 
+        : base(Fret.Set(-1, 1..5), strCount)
+    {
+    }
 }
